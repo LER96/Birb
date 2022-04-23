e6a8018e9855e9c9d750c04fef47662ee3bb0559
Alon Shani <alonshani16@gmail.com>
2022-04-23 19:18:26 +0300

    Added bird destruction

---
+++ Assets/Scrips/Bird.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            StartCoroutine(DestroyBird());
+        }
+        else if (collision.contacts[0].normal.y < 0.5)
+        {
+            StartCoroutine(DestroyBird());
+        }
+
+        IEnumerator DestroyBird()
+        {
+            yield return new WaitForSeconds(1.3f);
+            Destroy(gameObject);
+        }
+    }
+}
+
